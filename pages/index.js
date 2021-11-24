1f3ceb84c60489cf3d9b4c93bf8a117e25361d59
Joe McCann <joemccann@Joes-MacBook-Pro.local>
2021-11-23 21:54:20 -0600

    user profile added without styling

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,70 @@
 import React from 'react';
+import Image from 'next/image';
+
+import styled from 'styled-components';
+
+import userImage from '../public/images/image-jeremy.png';
 
 // font-family: 'Rubik', sans-serif;
 
 export default function Home() {
-	return <></>;
+	const Container = styled.div`
+		display: grid;
+		justify-content: center;
+	`;
+
+	const UserContainer = styled.div`
+		background: hsl(235, 46%, 20%);
+		height: 12rem;
+		width: 20rem;
+		border-radius: 10px;
+	`;
+
+	const UserProfile = styled.div`
+		background: hsl(246, 80%, 60%);
+		height: 7rem;
+		width: 20rem;
+		border-radius: 10px;
+	`;
+
+	const ProfileContainer = styled.div`
+		display: grid;
+		place-items: center;
+		grid-template-columns: 1fr 2fr;
+
+		padding: 0.8rem 2.5rem 0 2.5rem;
+	`;
+
+	const ProfileImage = styled(Image)`
+		border: 10px solid;
+		border-radius: 100%;
+	`;
+
+	const ProfileTitle = styled.p`
+		font-family: 'Rubik', sans-serif;
+	`;
+
+	const ProfileName = styled.p`
+		font-family: 'Rubik', sans-serif;
+	`;
+
+	return (
+		<>
+			<Container>
+				<UserContainer>
+					<UserProfile>
+						<ProfileContainer>
+							<div>
+								<ProfileImage src={userImage} height={70} width={70} />
+							</div>
+							<div>
+								<ProfileTitle>Report for</ProfileTitle>
+								<ProfileName>Jeremy Robinson</ProfileName>
+							</div>
+						</ProfileContainer>
+					</UserProfile>
+				</UserContainer>
+			</Container>
+		</>
+	);
 }
