5016d6721349538f1dff53d1e13381a9dc19b4ee
Joe McCann <joemccann@Joes-MacBook-Pro.local>
2021-11-24 12:57:53 -0600

    Moved stats styles to seperate file

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,18 @@ import React, { useState } from 'react';
 
 import styled from 'styled-components';
 
+import {
+	StatContainer,
+	SVGContainer,
+	StatProfileContainer,
+	CurrentContainer,
+	CardTitle,
+	CardHours,
+	PriorContainer,
+	Dots,
+	PriorHours,
+} from '../lib/styled-components/Stats.styled';
+
 import User from '../lib/components/User';
 
 // font-family: 'Rubik', sans-serif;
@@ -10,88 +22,6 @@ const Container = styled.div`
 	display: grid;
 	place-items: center;
 	margin-top: 2rem;
-`;
-
-const StatContainer = styled.div`
-	position: relative;
-	background: hsl(15, 100%, 70%);
-	height: 11rem;
-	width: 20rem;
-	margin-top: 1.5rem;
-	border-radius: 15px;
-`;
-
-const SVGContainer = styled.div`
-	display: grid;
-	justify-items: end;
-	z-index: 0;
-`;
-
-const StatProfileContainer = styled.div`
-	display: grid;
-	grid-template-columns: 1fr 1fr;
-	place-items: center;
-	position: absolute;
-	bottom: 0;
-	height: 8rem;
-	width: 20rem;
-	border-radius: 15px;
-	padding-top: 1rem;
-	background: hsl(235, 46%, 20%);
-`;
-
-const CurrentContainer = styled.div`
-	display: flex;
-	flex-direction: column;
-	justify-content: space-evenly;
-	align-items: start;
-	height: 100%;
-`;
-
-const CardTitle = styled.p`
-	margin: 0;
-	height: 10%;
-	font-family: 'Rubik', sans-serif;
-	font-weight: 500;
-	font-size: 18px;
-	color: hsl(236, 100%, 87%);
-`;
-
-const CardHours = styled.h1`
-	margin: 0;
-	height: 45%;
-	font-family: 'Rubik', sans-serif;
-	font-weight: 300;
-	color: hsl(236, 100%, 87%);
-`;
-
-const PriorContainer = styled.div`
-	display: flex;
-	flex-direction: column;
-	justify-content: space-evenly;
-	align-items: end;
-	height: 100%;
-`;
-
-const Dots = styled.p`
-	display: flex;
-	justify-content: end;
-	align-items: center;
-	height: 50%;
-	margin: 0;
-	font-family: 'Rubik', sans-serif;
-	font-weight: 500;
-	font-size: 1.5rem;
-	color: hsl(236, 100%, 87%);
-`;
-
-const PriorHours = styled.p`
-	height: 50%;
-	margin: 0;
-	font-family: 'Rubik', sans-serif;
-	font-weight: 300;
-	font-size: 1rem;
-	color: hsl(236, 100%, 87%);
 `;
 
 export default function Home() {
