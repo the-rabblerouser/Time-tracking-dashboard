f985fb06fb937e95d8fc8fc2eff6710640256da4
Joe McCann <joemccann@Joes-MacBook-Pro.local>
2021-11-23 20:41:40 -0600

    google font added

--- pages/_document.js
+++ pages/_document.js
@@ -36,6 +36,10 @@ class MyDocument extends Document {
 					<link rel='icon' href='/favicon.ico' />
 					<link rel='preconnect' href='https://fonts.googleapis.com' />
 					<link rel='preconnect' href='https://fonts.gstatic.com' crossorigin />
+					<link
+						href='https://fonts.googleapis.com/css2?family=Rubik:wght@300;400;500&display=swap'
+						rel='stylesheet'
+					/>
 				</Head>
 				<body>
 					<Main />
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+// font-family: 'Rubik', sans-serif;
+
 export default function Home() {
 	return <></>;
 }
