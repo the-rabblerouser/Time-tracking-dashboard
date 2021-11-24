1f7fc73aec20da32ee6f3e6f28526eb7fa4dc009
Joe McCann <joemccann@Joes-MacBook-Pro.local>
2021-11-24 09:27:36 -0600

    moved Container styled-component outside Home page function

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,14 @@ import User from '../lib/components/User';
 
 // font-family: 'Rubik', sans-serif;
 
+const Container = styled.div`
+	display: grid;
+	place-items: center;
+`;
+
 export default function Home() {
 	const [selected, setSelected] = useState('Daily');
 	const [buttons] = useState(['Daily', 'Weekly', 'Monthly']);
-
-	const Container = styled.div`
-		display: grid;
-		place-items: center;
-	`;
 
 	return (
 		<>
