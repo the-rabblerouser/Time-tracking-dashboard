3621e639f210e2e043b63e363b1c55372ada5090
Joe McCann <joemccann@Joes-MacBook-Pro.local>
2021-11-23 23:42:28 -0600

    Components, style-components and design folders moved to /lib folder with updated imports. User moved to its own component file

--- pages/index.js
+++ pages/index.js
@@ -1,54 +1,24 @@
 import React, { useState } from 'react';
-import Image from 'next/image';
 
-import {
-	Container,
-	UserContainer,
-	UserProfile,
-	ProfileContainer,
-	ImageContainer,
-	ProfileTitle,
-	ProfileName,
-	DateContainer,
-	DateButton,
-} from '../styled-components/UserProfile.styled';
+import styled from 'styled-components';
 
-import userImage from '../public/images/image-jeremy.png';
+import User from '../lib/components/User';
 
 // font-family: 'Rubik', sans-serif;
 
 export default function Home() {
 	const [selected, setSelected] = useState('Daily');
-	const [buttons, setButtons] = useState(['Daily', 'Weekly', 'Monthly']);
+	const [buttons] = useState(['Daily', 'Weekly', 'Monthly']);
 
-	const buttonToggle = (name) => setSelected(name);
+	const Container = styled.div`
+		display: grid;
+		place-items: center;
+	`;
+
 	return (
 		<>
 			<Container>
-				<UserContainer>
-					<UserProfile>
-						<ProfileContainer>
-							<ImageContainer>
-								<Image src={userImage} height={70} width={70} />
-							</ImageContainer>
-							<div>
-								<ProfileTitle>Report for</ProfileTitle>
-								<ProfileName>Jeremy Robson</ProfileName>
-							</div>
-						</ProfileContainer>
-					</UserProfile>
-					<DateContainer>
-						{buttons.map((btn, i) => (
-							<DateButton
-								key={btn}
-								selected={selected}
-								button={buttons[i]}
-								onClick={() => buttonToggle(btn)}>
-								{btn}
-							</DateButton>
-						))}
-					</DateContainer>
-				</UserContainer>
+				<User selected={selected} setSelected={setSelected} buttons={buttons} />
 			</Container>
 		</>
 	);
