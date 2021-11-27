2e226b6ea36f2eec8b0251116612ca35a03b0f3f
Joe McCann <joemccann@Joes-MacBook-Pro.local>
2021-11-27 00:11:27 -0600

    update to Stat components color to remove brackets

--- pages/index.js
+++ pages/index.js
@@ -61,37 +61,37 @@ export default function Home() {
 						selected={selected}
 						data={data[0]}
 						svg={WorkSVG()}
-						color={'hsl(15, 100%, 70%)'}
+						color='hsl(15, 100%, 70%)'
 					/>
 					<Stat
 						selected={selected}
 						data={data[1]}
 						svg={PlaySVG()}
-						color={'hsl(195, 74%, 62%)'}
+						color='hsl(195, 74%, 62%)'
 					/>
 					<Stat
 						selected={selected}
 						data={data[2]}
 						svg={StudySVG()}
-						color={'hsl(348, 100%, 68%)'}
+						color='hsl(348, 100%, 68%)'
 					/>
 					<Stat
 						selected={selected}
 						data={data[3]}
 						svg={ExerciseSVG()}
-						color={'hsl(145, 58%, 55%)'}
+						color='hsl(145, 58%, 55%)'
 					/>
 					<Stat
 						selected={selected}
 						data={data[4]}
 						svg={SocialSVG()}
-						color={'hsl(264, 64%, 52%)'}
+						color='hsl(264, 64%, 52%)'
 					/>
 					<Stat
 						selected={selected}
 						data={data[5]}
 						svg={SelfCareSVG()}
-						color={'hsl(43, 84%, 65%)'}
+						color='hsl(43, 84%, 65%)'
 					/>
 				</AllStatsContainer>
 			</Container>
